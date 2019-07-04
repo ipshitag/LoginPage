4d0de9f9489af082416ecd259c31750cc82530a9
Shivam gupta <34094754+shivadav@users.noreply.github.com>
2019-07-04 13:03:00 +0530

    Update verify.php

--- verify.php
+++ verify.php
@@ -5,7 +5,7 @@ if(isset($_POST))
 {
   $username = $_POST['username'];
   $password = $_POST['password'];
- // $password = sha1($password);
+  $password = sha1($password);
   
   $result = mysqli_query($con,"select * from users where username='$username' and password ='$password'") 
 or die("Failed to connect to database".mysql_error());
